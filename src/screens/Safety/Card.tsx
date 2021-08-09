18f1b69cd60f44c305059e78a3823d9dcae7807c
Christopher Krogh <chris.krogh@outlook.com>
2021-08-08 23:39:26 -0400

    carousel

---
+++ src/screens/Safety/Card.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Dimensions } from 'react-native';
+import styled from 'styled-components';
+
+import Column from '../../components/Column';
+import Spacer from '../../components/Spacer';
+import Typography from '../../components/Typography';
+import { Card } from './utils';
+
+const RAIL_SPACING = 16;
+
+export const CARD_WIDTH = Dimensions.get('window').width - 2 * RAIL_SPACING;
+export const CARD_HEIGHT = 400;
+
+const Container = styled(Column)`
+  padding: 16px ${RAIL_SPACING}px;
+  width: ${CARD_WIDTH}px;
+  height: ${CARD_HEIGHT}px;
+  background-color: ${({ theme }) =>
+    theme.colors.background.primary.toString()};
+  border-radius: 16px;
+  box-shadow: 0 2px 4px ${({ theme }) => theme.colors.text.secondary.toString()};
+`;
+
+type Props = {
+  item: Card;
+};
+
+const CardComponent: React.FC<Props> = ({ item }) => {
+  const { Icon, title, description } = item;
+
+  return (
+    <Container justifyContent="center" alignItems="center">
+      <Icon height={200} maxWidth={Dimensions.get('window').width - 64} />
+      <Spacer height={16} />
+      <Typography tag="h4" textAlign="center">
+        {title}
+      </Typography>
+      <Spacer height={4} />
+      <Typography tag="p" textAlign="center">
+        {description}
+      </Typography>
+    </Container>
+  );
+};
+
+export default CardComponent;
--- src/screens/Safety/Safety.tsx
+++ src/screens/Safety/Safety.tsx
@@ -1,23 +1,21 @@
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
-import { useWindowDimensions } from 'react-native';
+import { Dimensions, View } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
 import styled from 'styled-components';
 
 import Button from '../../components/Button';
 import Column from '../../components/Column';
-import Row from '../../components/Row';
 import Screen from '../../components/Screen';
 import Spacer from '../../components/Spacer';
-import SafetyPaymentsIcon from '../../icons/SafetyPaymentsIcon';
-import SafetyPersonalIcon from '../../icons/SafetyPersonalIcon';
-import SafetySavingIcon from '../../icons/SafetySavingIcon';
+import Typography from '../../components/Typography';
 import { SCREENS } from '../../navigation/utils/enums/screens';
 import { SignUpStackParamList } from '../../navigation/utils/screenConfigs/SignUpStack';
-import Paragraph from './Paragraph';
+import Card, { CARD_HEIGHT, CARD_WIDTH } from './Card';
+import { CARDS } from './utils';
 
 const RAIL_SPACING = 16;
-const MIDDLE_SPACER_WIDTH = 16;
 
 const Container = styled(Column)`
   padding: 0 ${RAIL_SPACING}px;
@@ -25,49 +23,34 @@ const Container = styled(Column)`
   height: 100%;
 `;
 
+const CarouselWrapper = styled(View)`
+  height: ${CARD_HEIGHT + 64}px;
+`;
+
 const Safety: React.FC = () => {
   const navigation =
     useNavigation<StackNavigationProp<SignUpStackParamList, SCREENS.SAFETY>>();
 
-  const { width } = useWindowDimensions();
-
   const goToBiometrics = () => navigation.push(SCREENS.BIOMETRICS);
-
-  const ICON_WIDTH = Math.min(
-    Math.floor((width - 2 * RAIL_SPACING - 0.5 * MIDDLE_SPACER_WIDTH) / 2),
-    220,
-  );
 
   return (
     <Screen>
-      <Container justifyContent="space-around" alignItems="center">
-        <Row fullWidth>
-          <SafetyPaymentsIcon width={ICON_WIDTH} maxHeight={150} />
-          <Spacer width={MIDDLE_SPACER_WIDTH} />
-          <Paragraph
-            width={ICON_WIDTH}
-            title="Safe"
-            description="Now it's easier than ever before to send money, but that doesn't mean it shouldn't be safe. Dot utilizes a myriad of safety features to process transactions."
+      <Container alignItems="center">
+        <Typography tag="h3" textAlign="center">
+          Security is our priority
+        </Typography>
+        <Spacer height={16} />
+        <CarouselWrapper>
+          <Carousel
+            layout="tinder"
+            data={CARDS}
+            renderItem={Card}
+            sliderWidth={Dimensions.get('window').width}
+            sliderHeight={CARD_HEIGHT}
+            itemWidth={CARD_WIDTH}
+            itemHeight={CARD_HEIGHT}
           />
-        </Row>
-        <Row fullWidth>
-          <Paragraph
-            width={ICON_WIDTH}
-            title="Secure"
-            description="PCI-DSS certified means that we protect all of your data. Dot allows you to move money around with confidence."
-          />
-          <Spacer width={MIDDLE_SPACER_WIDTH} />
-          <SafetyPersonalIcon width={ICON_WIDTH} maxHeight={150} />
-        </Row>
-        <Row fullWidth>
-          <SafetySavingIcon width={ICON_WIDTH} maxHeight={150} />
-          <Spacer width={MIDDLE_SPACER_WIDTH} />
-          <Paragraph
-            width={ICON_WIDTH}
-            title="Robust"
-            description="Security is our top priority and we invest a lot of resources ensuring that Dot protects its customers."
-          />
-        </Row>
+        </CarouselWrapper>
         <Button onPress={goToBiometrics}>Continue</Button>
       </Container>
     </Screen>
