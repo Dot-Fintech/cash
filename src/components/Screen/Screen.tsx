7e29e15dc04f1f6ca9b13254ac7a9038a2983ebe
Christopher Krogh <chris.krogh@outlook.com>
2021-08-08 20:37:11 -0400

    using window dimensions for screen width and height

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Dimensions } from 'react-native';
 import { SafeAreaView, View } from 'react-native';
 import styled, { css } from 'styled-components';
 
 const styles = css`
-  width: 100%;
-  height: 100%;
+  width: ${Dimensions.get('window').width}px;
+  height: ${Dimensions.get('window').height}px;
   background-color: ${({ theme }) =>
     theme.colors.background.primary.toString()};
 `;
