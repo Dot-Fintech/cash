8e37849265c0da7f6493f5359bfecb7f136684dc
Christopher Krogh <chris.krogh@outlook.com>
2021-08-08 22:42:01 -0400

    lock screen in portrait

--- App.tsx
+++ App.tsx
@@ -5,8 +5,10 @@ import { Roboto_400Regular } from '@expo-google-fonts/roboto';
 import { SourceSansPro_400Regular } from '@expo-google-fonts/source-sans-pro';
 import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
+import * as ScreenOrientation from 'expo-screen-orientation';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { useEffect } from 'react';
 
 import RootStack from './src/navigation/RootStack';
 import Providers from './src/Providers';
@@ -17,6 +19,14 @@ const App: React.FC = () => {
     Roboto_400Regular,
     SourceSansPro_400Regular,
   });
+
+  useEffect(() => {
+    (async () => {
+      await ScreenOrientation.lockAsync(
+        ScreenOrientation.OrientationLock.PORTRAIT_UP,
+      );
+    })();
+  }, []);
 
   return fontsLoaded ? (
     <Providers>
