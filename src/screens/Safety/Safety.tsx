5fde3b0c13ee9a4dc0f03713bd4ba06c8a26a5bc
Christopher Krogh <chris.krogh@outlook.com>
2021-08-08 20:37:44 -0400

    safety page

--- src/screens/Safety/Safety.tsx
+++ src/screens/Safety/Safety.tsx
@@ -1,22 +1,75 @@
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
-import { Button, Text } from 'react-native';
+import { useWindowDimensions } from 'react-native';
+import styled from 'styled-components';
 
+import Button from '../../components/Button';
+import Column from '../../components/Column';
+import Row from '../../components/Row';
 import Screen from '../../components/Screen';
+import Spacer from '../../components/Spacer';
+import SafetyPaymentsIcon from '../../icons/SafetyPaymentsIcon';
+import SafetyPersonalIcon from '../../icons/SafetyPersonalIcon';
+import SafetySavingIcon from '../../icons/SafetySavingIcon';
 import { SCREENS } from '../../navigation/utils/enums/screens';
 import { SignUpStackParamList } from '../../navigation/utils/screenConfigs/SignUpStack';
+import Paragraph from './Paragraph';
+
+const RAIL_SPACING = 16;
+const MIDDLE_SPACER_WIDTH = 16;
+
+const Container = styled(Column)`
+  padding: 0 ${RAIL_SPACING}px;
+  width: 100%;
+  height: 100%;
+`;
 
 const Safety: React.FC = () => {
   const navigation =
     useNavigation<StackNavigationProp<SignUpStackParamList, SCREENS.SAFETY>>();
 
+  const { width } = useWindowDimensions();
+
   const goToBiometrics = () => navigation.push(SCREENS.BIOMETRICS);
+
+  const ICON_WIDTH = Math.min(
+    Math.floor((width - 2 * RAIL_SPACING - 0.5 * MIDDLE_SPACER_WIDTH) / 2),
+    220,
+  );
 
   return (
     <Screen>
-      <Text>Safety</Text>
-      <Button title="Biometrics" onPress={goToBiometrics} />
+      <Container justifyContent="space-around" alignItems="center">
+        <Row fullWidth>
+          <SafetyPaymentsIcon width={ICON_WIDTH} maxHeight={150} />
+          <Spacer width={MIDDLE_SPACER_WIDTH} />
+          <Paragraph
+            width={ICON_WIDTH}
+            title="Safe"
+            description="Now it's easier than ever before to send money, but that doesn't mean it shouldn't be safe. Dot utilizes a myriad of safety features to process transactions."
+          />
+        </Row>
+        <Row fullWidth>
+          <Paragraph
+            width={ICON_WIDTH}
+            title="Secure"
+            description="PCI-DSS certified means that we protect all of your data. Dot allows you to move money around with confidence."
+          />
+          <Spacer width={MIDDLE_SPACER_WIDTH} />
+          <SafetyPersonalIcon width={ICON_WIDTH} maxHeight={150} />
+        </Row>
+        <Row fullWidth>
+          <SafetySavingIcon width={ICON_WIDTH} maxHeight={150} />
+          <Spacer width={MIDDLE_SPACER_WIDTH} />
+          <Paragraph
+            width={ICON_WIDTH}
+            title="Robust"
+            description="Security is our top priority and we invest a lot of resources ensuring that Dot protects its customers."
+          />
+        </Row>
+        <Button onPress={goToBiometrics}>Continue</Button>
+      </Container>
     </Screen>
   );
 };
