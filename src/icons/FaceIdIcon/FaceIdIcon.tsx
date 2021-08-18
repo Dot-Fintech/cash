13c91ae6ffd8ccd001fb749dc0838a85016dc20a
Christopher Krogh <chris.krogh@outlook.com>
2021-08-18 15:50:37 -0400

    fixed icons

--- src/icons/FaceIdIcon/FaceIdIcon.tsx
+++ src/icons/FaceIdIcon/FaceIdIcon.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { SvgXml } from 'react-native-svg';
+import { useTheme } from 'styled-components';
 
 import { DesiredIconDimensions } from '../types';
 import { getIconDimensions } from '../utils';
-import { xml } from './xml';
+import { buildXml } from './xml';
 
 const FaceIdIcon: React.FC<DesiredIconDimensions> = (props) => {
+  const theme = useTheme();
   const dimensions = getIconDimensions({ AR: 1, ...props });
-  return <SvgXml xml={xml} {...dimensions} />;
+  return <SvgXml xml={buildXml(theme.colors.text.primary)} {...dimensions} />;
 };
 
 export default FaceIdIcon;
--- src/icons/FingerprintIcon/FingerprintIcon.tsx
+++ src/icons/FingerprintIcon/FingerprintIcon.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { SvgXml } from 'react-native-svg';
+import { useTheme } from 'styled-components';
 
 import { DesiredIconDimensions } from '../types';
 import { getIconDimensions } from '../utils';
-import { xml } from './xml';
+import { buildXml } from './xml';
 
 const FingerprintIcon: React.FC<DesiredIconDimensions> = (props) => {
+  const theme = useTheme();
   const dimensions = getIconDimensions({ AR: 362 / 512, ...props });
-  return <SvgXml xml={xml} {...dimensions} />;
+  return <SvgXml xml={buildXml(theme.colors.text.primary)} {...dimensions} />;
 };
 
 export default FingerprintIcon;
